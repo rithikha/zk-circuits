5c8c47b121a32843854ea00f050110f957b88304
rithikha <rithikha.rajamohan@gmail.com>
2025-02-19 14:15:53 -0500

    fix vit MIME type w manual WASM instantiation

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ import main from "./circuit/src/main.nr?url";
 import nargoToml from "./circuit/Nargo.toml?url";
 import { UltraHonkBackend } from '@aztec/bb.js';
 import { Noir } from '@noir-lang/noir_js';
+import initNoirC from "@noir-lang/noirc_abi";
+import initACVM from "@noir-lang/acvm_js";
+import acvm from "@noir-lang/acvm_js/web/acvm_js_bg.wasm?url";
+import noirc from "@noir-lang/noirc_abi/web/noirc_abi_wasm_bg.wasm?url";
+await Promise.all([initACVM(fetch(acvm)), initNoirC(fetch(noirc))]);
 
 export async function getCircuit() {
     const fm = createFileManager("/");
