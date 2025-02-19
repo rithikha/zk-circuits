2d56c3a9bd50b29524bb7f98dde9bc2532306fad
rithikha <rithikha.rajamohan@gmail.com>
2025-02-19 14:28:38 -0500

    generate witness & proof for age verification

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 
 import { compile, createFileManager } from "@noir-lang/noir_wasm"
 
-import main from "./circuit/src/main.nr?url";
-import nargoToml from "./circuit/Nargo.toml?url";
 import { UltraHonkBackend } from '@aztec/bb.js';
 import { Noir } from '@noir-lang/noir_js';
 import initNoirC from "@noir-lang/noirc_abi";
@@ -10,6 +8,9 @@ import initACVM from "@noir-lang/acvm_js";
 import acvm from "@noir-lang/acvm_js/web/acvm_js_bg.wasm?url";
 import noirc from "@noir-lang/noirc_abi/web/noirc_abi_wasm_bg.wasm?url";
 await Promise.all([initACVM(fetch(acvm)), initNoirC(fetch(noirc))]);
+
+import main from "./circuit/src/main.nr?url";
+import nargoToml from "./circuit/Nargo.toml?url";
 
 export async function getCircuit() {
     const fm = createFileManager("/");
@@ -20,7 +21,6 @@ export async function getCircuit() {
     fm.writeFile("./Nargo.toml", nargoTomlBody);
     return await compile(fm);
 }
-
 
 const show = (id, content) => {
     const container = document.getElementById(id);
