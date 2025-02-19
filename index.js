ea8be1728c270644e0d86b050560883ed84bc4f6
rithikha <rithikha.rajamohan@gmail.com>
2025-02-19 14:10:47 -0500

    add noir imports

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+
+import { compile, createFileManager } from "@noir-lang/noir_wasm"
+
+import main from "./circuit/src/main.nr?url";
+import nargoToml from "./circuit/Nargo.toml?url";
+import { UltraHonkBackend } from '@aztec/bb.js';
+import { Noir } from '@noir-lang/noir_js';
+
+export async function getCircuit() {
+    const fm = createFileManager("/");
+    const { body } = await fetch(main);
+    const { body: nargoTomlBody } = await fetch(nargoToml);
+
+    fm.writeFile("./src/main.nr", body);
+    fm.writeFile("./Nargo.toml", nargoTomlBody);
+    return await compile(fm);
+}
+
+
 const show = (id, content) => {
     const container = document.getElementById(id);
     container.appendChild(document.createTextNode(content));
